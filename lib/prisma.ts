8c803fc8d4c6dda016824204aa8287157b638ba5
neosoda <neosoda@gmail.com>
2026-06-07 12:46:14 +0200

    fix(prisma): lazy init db client for next build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -19,6 +19,25 @@ function createPrismaClient() {
   })
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+function getPrismaClient() {
+  if (globalForPrisma.prisma) {
+    return globalForPrisma.prisma
+  }
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+  const client = createPrismaClient()
+
+  if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = client
+  }
+
+  return client
+}
+
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_target, property, receiver) {
+    const client = getPrismaClient()
+    const value = Reflect.get(client as object, property, receiver)
+
+    return typeof value === 'function' ? value.bind(client) : value
+  },
+})
